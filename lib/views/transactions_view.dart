58094cc933cdee90cf23d428412bd992da69dae0
Robert Morales <lsurec@demosoft.com.gt>
2024-10-23 15:06:56 -0600

    "Added app theme import and styling, modified TransactionsView to include app bar, balance display, and refresh indicator"

--- lib/views/transactions_view.dart
+++ lib/views/transactions_view.dart
@@ -1,3 +1,4 @@
+import 'package:fl_colegio/theme/app_theme.dart';
 import 'package:fl_colegio/widgets/widgets.dart';
 import 'package:flutter/material.dart';
 
@@ -6,17 +7,66 @@ class TransactionsView extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    var textStyleBold = const TextStyle(
+      color: AppTheme.secondaryColorDark,
+      fontSize: 17,
+      fontWeight: FontWeight.bold,
+    );
+
     return Scaffold(
-      body: ListView.builder(
-        itemCount: 50,
-        itemBuilder: (BuildContext context, int index) {
-          return const CardHistoryWidget(
-            title: "Ad qui minim exercitation incididunt elit est.",
-            date: "01/01/2023",
-            transaction: 1,
-            mount: 100.00,
-          );
-        },
+      appBar: AppBar(),
+      body: Column(
+        children: [
+          Container(
+            padding: EdgeInsets.all(20),
+            height: 150,
+            child: Column(
+              children: [
+                Center(
+                  child: Text(
+                    "Saldo: 2,000.00",
+                    style: textStyleBold,
+                  ),
+                ),
+                const SizedBox(height: 5),
+                Center(
+                  child: Text(
+                    "Insolvente",
+                    style: textStyleBold,
+                  ),
+                ),
+                Center(
+                  child: Text(
+                    "Solvente",
+                    style: textStyleBold,
+                  ),
+                )
+              ],
+            ),
+          ),
+          Expanded(
+            child: RefreshIndicator(
+              onRefresh: () async {},
+              child: ListView.builder(
+                itemCount: 10,
+                itemBuilder: (BuildContext context, int index) {
+                  return Container(
+                    margin: const EdgeInsets.symmetric(
+                      horizontal: 20,
+                      vertical: 5,
+                    ),
+                    child: const CardHistoryWidget(
+                      title: "Ad qui minim exercitation incididunt elit est.",
+                      date: "01/01/2023",
+                      transaction: 1,
+                      mount: 100.00,
+                    ),
+                  );
+                },
+              ),
+            ),
+          ),
+        ],
       ),
     );
   }
