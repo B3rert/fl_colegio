1b9d377e69a29f9c640a522e948b0b00ece7c321
Robert Morales <lsurec@demosoft.com.gt>
2024-10-25 12:33:41 -0600

    "Added export statement for transaction_bank_model.dart"

--- lib/models/models.dart
+++ lib/models/models.dart
@@ -4,3 +4,4 @@ export 'package:fl_colegio/models/cuenta_model.dart';
 export 'package:fl_colegio/models/usuario_model.dart';
 export 'package:fl_colegio/models/login_model.dart';
 export 'package:fl_colegio/models/response_model.dart';
+export 'package:fl_colegio/models/transaction_bank_model.dart';
