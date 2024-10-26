e8025ee5ac0c4dc77173d87c74d16bfeee89577a
Robert Morales <lsurec@demosoft.com.gt>
2024-10-25 22:47:56 -0600

    "Updated app icons, API provider URLs, and dependencies in pubspec files."

--- lib/providers/api_provider.dart
+++ lib/providers/api_provider.dart
@@ -1,4 +1,6 @@
 class ApiProvider {
-  final String baseUrlColegio = "http://192.168.100.8:9194/api/";
-  final String baseUrlbBanco = "http://192.168.100.8:9193/api/";
+  //  192.168.149.217
+  final String baseUrlColegio = "http://192.168.149.217:9194/api/";
+  final String baseUrlbBanco = "http://192.168.149.217:9193/api/";
+  // final String baseUrlbBanco = "http://192.168.100.8:9193/api/";
 }
